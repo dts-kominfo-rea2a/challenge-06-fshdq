a9f5553129ac71fdc2c51880857ac5247653ad4a
Farras Shiddiq <farrascitizen@gmail.com>
2022-06-08 11:03:44 +0700

    done

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require("fs");
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +19,25 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+function bacaData(fnCallBack){
+  const hasilAkhir = [];
+  fs.readFile(file1, "utf8", (err, data) => {
+    if (err) return fnCallBack(err, null);
+    const message1 = JSON.parse(data).message.split(" ");
+    hasilAkhir.push(message1[1]);
+    fs.readFile(file2, "utf-8", (err, data) => {
+      if (err) return fnCallBack(err, null);
+      const message2 = JSON.parse(data)[0].message.split(" ");
+      hasilAkhir.push(message2[1]);
+      fs.readFile(file3, "utf-8", (err, data) => {
+        if (err) return fnCallBack(err, null);
+        const message3 = JSON.parse(data)[0].data.message.split(" ");
+        hasilAkhir.push(message3[1]);
+        fnCallBack(null, hasilAkhir);
+      });
+    });
+  });
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
